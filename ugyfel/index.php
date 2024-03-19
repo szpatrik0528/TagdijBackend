1c86195cb05393b8efb26a0020ec654bd80e1780
szpatrik0528 <szpatrik.photoshop@gmail.com>
2024-03-19 09:48:55 +0100

    main

--- ugyfel/index.php
+++ ugyfel/index.php
@@ -8,14 +8,10 @@ switch ($_SERVER['REQUEST_METHOD']) {
         require_once 'ugyfel/postugyfel.php';
         break;
     case 'DELETE':
-<<<<<<< HEAD
         require_once 'ugyfel/deleteugyfelek.php';
         break;
     case 'PUT':
         require_once 'ugyfel/putugyfelek.php';
-=======
-        require_once 'ugyfel/deleteugyfel.php;
->>>>>>> 23a36b86046809d5871fd440ccec91e72ad97b84
         break;
     default:
         break;
