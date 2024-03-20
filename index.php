068d5c9e00d5a2e5928dacb9ce3eefe8176360d7
szpatrik0528 <124908897+szpatrik0528@users.noreply.github.com>
2024-03-20 10:26:09 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,24 @@
-<!DOCTYPE html>
-<!--
-fogadja az url kéréseket és megválaszolja azokat
-GET http://localhost/TagdijBackend/index.php?ugyfel -> minden ugyfel
-GET http://localhost/TagdijBackend/index.php?ugyfel/(id) -> adott id ugyfel
-POST http://localhost/TagdijBackend/index.php?ugyfel -> létrehoz ugyfelet
-PUT http://localhost/TagdijBackend/index.php?ugyfel/{id} -> modosit adott ugyfelet
-DELETE http://localhost/TagdijBackend/index.php?ugyfel/{id} -> torol adott ugyfelet
--->
-<html>
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <title>Tagdíj Backend</title>
-</head>
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 
-<body>
-    <pre>
-        <?php
-        //var_dump($_SERVER['REQUEST_METHOD']);
-        //var_dump($_SERVER['QUERY_STRING']);
-        $keresSzoveg = explode('/', $_SERVER['QUERY_STRING']);
-        if ($keresSzoveg[0] === 'ugyfel') {
-            require_once 'ugyfel/index.php';
-        } else {
-            http_response_code(404);
-            echo "nincs ilyen api";
-        }
-        ?>
-        </pre>
-</body>
-
-</html>
+/* fogadja az url kéréseket és megválaszolja azokat
+  GET http://localhost/TagdijBackend/index.php?ugyfel -> minden ugyfel
+  GET http://localhost/TagdijBackend/index.php?ugyfel/(id) -> adott id ugyfel
+  POST http://localhost/TagdijBackend/index.php?ugyfel -> létrehoz ugyfelet
+  PUT http://localhost/TagdijBackend/index.php?ugyfel/{id} -> modosit adott ugyfelet
+  DELETE http://localhost/TagdijBackend/index.php?ugyfel/{id} -> torol adott ugyfelet
+  // */
+//var_dump($_SERVER['REQUEST_METHOD']);
+//var_dump($_SERVER['QUERY_STRING']);
+$keresSzoveg = explode('/', $_SERVER['QUERY_STRING']);
+if ($keresSzoveg[0] === 'ugyfel') {
+    require_once 'ugyfel/index.php';
+} else {
+    http_response_code(404);
+    $errorJson = array("message" => 'Nincs ilyen api');
+    return json_encode($errorJson);
+}
